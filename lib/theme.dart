a110349327d06c9626f91440bd4d0dab0fd64922
Luca Oliveira <luca.oliveira@institutoiracema.org.br>
2024-05-15 20:12:27 -0300

    style: add fonts and colors

---
+++ lib/theme.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CustomColors {
+  static const Color primaryColor = Color(0xFFE70E02); // #E70E02
+  static const Color accentColor = Color(0xFFF42B03); // #F42B03
+  static const Color thirdColor = Color(0xFFD84A05); // #D84A05
+  static const Color fourthColor = Color(0xFFEC7505); // #EC7505
+  static const Color fifthColor = Color(0xFFE89005); // #E89005
+  static const Color sixthColor = Color(0xFFAC1304); // #AC1304
+  static const Color seventhColor = Color(0xFF474747); // #474747
+  static const Color eighthColor = Color(0xFF414141); // #414141
+}
